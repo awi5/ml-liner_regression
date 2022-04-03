d80335c3e1e9166700c971ee169d7e2a22059cc6
awi5 <102463024+awi5@users.noreply.github.com>
2022-04-03 15:13:36 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 16:31:37 2022
+
+@author: DELL
+"""
+
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor_loaded = data["model"]
+hours = data["hours"]
+
+def show_predict_page():
+    st.title("Student's Score Prediction")
+    st.write("""### We need some information to predict the marks""")
+    hrs = st.text_input("Enter Number of hours you study:", "")
+    ok = st.button("Calculate Marks")
+    if ok:
+        X = np.array([[hrs]])
+        X = X.astype(float)
+        
+        marks = regressor_loaded.predict(X)
+        st.subheader(f"The estimated Marks are {marks[0]:2f}")
+        
+show_predict_page()
+        
